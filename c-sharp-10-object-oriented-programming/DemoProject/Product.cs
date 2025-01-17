a94338036a4eda211a7defd0a0bf3ebbee2de514
Marcus Larsson <bayareaviking@hotmail.com>
2025-01-16 18:24:01 -0800

    Add files to the DemoProject

---
+++ c-sharp-10-object-oriented-programming/DemoProject/Product.cs
@@ -0,0 +1,19 @@
+﻿namespace BethanysPieShop.InventoryManagement
+{
+    public class Product // It's good practice to explicitly include your class access modifier ("public" in this case,) defaults to "internal"
+    {
+
+        private int id;
+        private string name = string.Empty;
+        private string? description;
+
+        private int maxItemsInStock = 0;
+
+        private UnitType unitType;
+        private int amountInStock = 0;
+        private bool isBelowStockTreshold = false;
+
+        //ToDo: add price value
+
+    }
+}
